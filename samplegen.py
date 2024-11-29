97a4bfadb38a939261eaf4bde28016526baf851f
matheucampbell <matheu.campbell.us@outlook.com>
2024-11-29 14:15:53 -0500

    normal distribution added

--- samplegen.py
+++ samplegen.py
@@ -1,4 +1,5 @@
 # Generate samples from a particular probability distribution
+import matplotlib.pyplot as plt
 import numpy as np
 from scipy import stats
 
@@ -16,6 +17,34 @@ class Uniform:
                 file.write(str(num) + '\n')
 
 
+class Normal:
+    def __init__(self, mean, variance):
+        self.mean = mean
+        self.var = variance
+    
+    def write_samples(self, N, fname):
+        with open(fname, 'w') as file:
+            samples = stats.norm.rvs(loc=self.mean, scale=self.var**0.5, size=N)
+            for num in samples:
+                file.write(str(num) + '\n')
+        
+        # plt.figure(figsize=(10, 6))
+        # plt.hist(samples, bins=30, density=True, alpha=0.6, color='blue', label='Sample Histogram')
+
+        # x = sorted(samples)
+        # pdf = stats.norm.pdf(x, loc=self.mean, scale=self.var**0.5)
+        # plt.plot(x, pdf, 'r-', lw=2, label='True Distribution')
+
+        # plt.title(f"Histogram and PDF of Generated Samples\n(Mean={self.mean}, Variance={self.var})")
+        # plt.xlabel("Value")
+        # plt.ylabel("Density")
+        # plt.legend()
+        # plt.grid(True)
+        # plt.show()
+
+
 if __name__ == '__main__':
-    dist = Uniform(0, 10)
-    dist.write_samples(10000, 'uniform10.txt')
+    uni_dist = Uniform(0, 10)
+    uni_dist.write_samples(10000, 'uniform10.txt')
+    norm_dist = Normal(500, 50)
+    norm_dist.write_samples(10000, 'norm500')
