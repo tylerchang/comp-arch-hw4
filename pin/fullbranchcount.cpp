aeae7f53f4c0655dc9a8160c8666bfdfd269b46c
ineskhou <ik2512@columbia.edu>
2024-11-30 22:22:37 -0500

    added a free, change image order

--- pin/fullbranchcount.cpp
+++ pin/fullbranchcount.cpp
@@ -10,9 +10,14 @@ using std::string;
 ofstream OutFile;
 
 // The running count of instructions is kept here
-// make it static to help the compiler optimize logbranch
-static UINT64 bcountTaken = 0;  // Conditional branches taken
-static UINT64 bcountNotTaken = 0; // Conditional branches not taken
+// make it static to help the compiler optimize docount
+static UINT64 bcountTaken = 0;  // Conditional branches Taken
+static UINT64 bcountNotTaken = 0; // Conditional branchec Not Taken
+static UINT64 ubcount = 0; // Unconditional branches
+
+
+// the imag here so its intalized right away
+IMG mainExecutableImg;
 
 typedef struct BranchInfo {
     ADDRINT instPtr;        // Instruction pointer
@@ -73,6 +78,13 @@ VOID Instruction(INS ins, VOID* v) {
     }
 }
 
+// not sure but internet suggests this might solve?
+VOID ImageLoad(IMG img, VOID* v) {
+    if (IMG_IsMainExecutable(img) && !mainExecutableImg.Valid()) {
+        mainExecutableImg = img;
+        cerr << "Main executable image loaded: " << IMG_Name(img) << endl;
+    }
+}
 
 KNOB< string > KnobOutputFile(KNOB_MODE_WRITEONCE, "pintool", "o", "outputs/branchcount.out", "specify output file name");
 
@@ -97,6 +109,13 @@ VOID SaveBranchListToFile(const char* filename) {
     fclose(file);
 }
 
+void FreeBranchList() {
+    while (branchListHead) {
+        BranchInfo* temp = branchListHead;
+        branchListHead = branchListHead->next;
+        free(temp);
+    }
+}
 
 
 // This function is called when the application exits
@@ -104,11 +123,14 @@ VOID Fini(INT32 code, VOID* v)
 {
     // Write to a file since cout and cerr maybe closed by the application
     OutFile.setf(ios::showbase);
-    OutFile << "Conditional Branches Taken: " << bcountTaken << endl;
-    OutFile << "Conditional Branches Not Taken: " << bcountNotTaken << endl;
+    OutFile << "Conditional Branch Count: " << (bcountTaken + bcountNotTaken) << endl;
+    OutFile << "Taken Braches: " << bcountTaken << endl;
+    OutFile << "Taken Braches: " << bcountNotTaken << endl;
+    OutFile << "Unconditional Branch Count: " << ubcount << endl;
     OutFile.close();
 
-    SaveBranchListToFile("outputs/branchlog.txt");
+    SaveBranchListToFile("branches_taken.txt")
+    FreeBranchList();
 }
 
 /* ===================================================================== */
@@ -134,6 +156,8 @@ int main(int argc, char* argv[])
     if (PIN_Init(argc, argv)) return Usage();
 
     OutFile.open(KnobOutputFile.Value().c_str());
+
+    IMG_AddInstrumentFunction(ImageLoad, 0);
 
     // Register Instruction to be called to instrument instructions
     INS_AddInstrumentFunction(Instruction, 0);
