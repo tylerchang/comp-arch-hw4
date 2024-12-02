0763cb8d2eaf64b1ae7af990328577041f692d42
matheucampbell <matheu.campbell.us@outlook.com>
2024-12-02 12:56:07 -0500

    renamed

--- pin/simplebranch.c
+++ pin/simplebranch.c
@@ -6,6 +6,7 @@ int main(){
     // 11 checks for i < lim (1 branch taken when i = lim)
     // 10 checks for i > m   (4 taken for 6 <= i < 10 )
     // 21 total: 5 taken
+    
     int lim = 10;
     int m = 5;
     int n = 0;
