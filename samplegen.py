d7ee7ded49523a3a549f853d481b0a6f78d6bfc7
matheucampbell <matheu.campbell.us@outlook.com>
2024-12-05 19:19:03 -0500

    skewnorm dist added

--- samplegen.py
+++ samplegen.py
@@ -43,8 +43,37 @@ class Normal:
         # plt.show()
 
 
+class SkewNorm:
+    def __init__(self, mean, variance, skewness):
+        self.mean = mean
+        self.var = variance
+        self.skew = skewness
+
+    def write_samples(self, N, fname):
+        with open(fname, 'w') as file:
+            samples = stats.skewnorm.rvs(a=self.skew, loc=self.mean, scale=self.var**0.5, size=N)
+            for num in samples:
+                file.write(str(num) + '\n')
+        
+        plt.figure(figsize=(10, 6))
+        plt.hist(samples, bins=30, density=True, alpha=0.6, color='blue', label='Sample Histogram')
+
+        x = sorted(samples)
+        pdf = stats.skewnorm.pdf(x, a=self.skew, loc=self.mean, scale=self.var**0.5)
+        plt.plot(x, pdf, 'r-', lw=2, label='True Distribution')
+
+        plt.title(f"Histogram and PDF of Generated Samples\n(Mean={self.mean}, Variance={self.var})")
+        plt.xlabel("Value")
+        plt.ylabel("Density")
+        plt.legend()
+        plt.grid(True)
+        plt.show()
+
+
 if __name__ == '__main__':
-    uni_dist = Uniform(0, 10)
-    uni_dist.write_samples(10000, 'uniform10.txt')
-    norm_dist = Normal(500, 50)
-    norm_dist.write_samples(10000, 'norm500')
+    # uni_dist = Uniform(0, 10)
+    # uni_dist.write_samples(10000, 'uniform10.txt')
+    # norm_dist = Normal(500, 50)
+    # norm_dist.write_samples(10000, 'norm500.txt')
+    skew_dist = SkewNorm(500, 100, 5)
+    skew_dist.write_samples(100000, 'skewnorm5.txt')
